20576527331e0f1140d6a09c4e397e8c1065260d
YeruhamMendelson <y0583207549@gmail.com>
2025-10-22 12:21:14 +0300

    bottun for mark as done - finish step1

--- script.js
+++ script.js
@@ -1,66 +1,79 @@
 function createTaskElement(text){
-    const newTaskElement  = document.createElement("span")
-    newTaskElement.innerText = text
-    newTaskElement.className = "text-task"
-    return newTaskElement
+    const newTaskElement  = document.createElement("span");
+    newTaskElement.innerText = text;
+    newTaskElement.className = "text-task";
+    return newTaskElement;
 }
 
 
-function createDeleteElement(){
-    const newDeleteElement = document.createElement("button")
-    newDeleteElement.setAttribute("type", "submit")
-    newDeleteElement.className = "delete-task"
-    newDeleteElement.innerText = "delete"
-    newDeleteElement.addEventListener('click', (e) => {  e.target.parentElement.remove() })
-    return newDeleteElement
+function createDeleteButton(){
+    const newDeleteElement = document.createElement("button");
+    newDeleteElement.setAttribute("type", "submit");
+    newDeleteElement.className = "delete-task";
+    newDeleteElement.innerText = "delete";
+    newDeleteElement.addEventListener('click', (e) => {  e.target.parentElement.remove(); })
+    return newDeleteElement;
+}
+
+
+function createDoneButton(){
+    const newDoneElement = document.createElement("button");
+    newDoneElement.setAttribute("type", "submit");
+    newDoneElement.className = "done-task";
+    newDoneElement.innerText = "mark as done";
+    newDoneElement.addEventListener('click', (e) => {  e.target.className="done"; e.target.innerText="done"; })
+    return newDoneElement;
 }
 
 
 function createFullTaskElement(text){
-    const taskElement = createTaskElement(text)
-    const deleteElement = createDeleteElement()
+    const taskElement = createTaskElement(text);
+     const deleteElement = createDeleteButton();
+    const doneElement = createDoneButton();
+   
 
-    const fullTaskElement = document.createElement("li")
-    fullTaskElement.className = "task"
+    const fullTaskElement = document.createElement("li");
+    fullTaskElement.className = "task";
     
-    fullTaskElement.appendChild(taskElement)
-    fullTaskElement.appendChild(deleteElement)
+    fullTaskElement.appendChild(taskElement);
+    fullTaskElement.appendChild(deleteElement);
+    fullTaskElement.appendChild(doneElement);
 
-    return fullTaskElement
+    return fullTaskElement;
 }
 
 
 
 function readInput(id){
-    const taskInput = document.getElementById(id)
-    const value = taskInput.value
-    taskInput.value = ""
-    return value
+    const taskInput = document.getElementById(id);
+    const value = taskInput.value;
+    taskInput.value = "";
+    return value;
 }
 
 
 function addNewTask(inputId, listTaskId){
-    const newTask = readInput(inputId)
+    const newTask = readInput(inputId);
     if (!newTask){
-        return
+        return;
     }
-    const listTasks = document.getElementById(listTaskId)
-    const newTaskElement = createFullTaskElement(newTask)
-    listTasks.appendChild(newTaskElement)
+    const listTasks = document.getElementById(listTaskId);
+    const newTaskElement = createFullTaskElement(newTask);
+    listTasks.appendChild(newTaskElement);
 }
 
 
 function addTaskByEnter(key, inputId, listTaskId){
     if (key != "Enter"){
-        return
+        return;
     }
-    addNewTask( inputId, listTaskId)
+    addNewTask( inputId, listTaskId);
 }
 
 
-const saveNewTask = document.getElementById("add-button")
-saveNewTask.addEventListener('click', () => { addNewTask("input-task", "list-tasks", Date.now()) })
+const saveNewTask = document.getElementById("add-button");
+saveNewTask.addEventListener('click', () => { addNewTask("input-task", "list-tasks") });
 
 
-const inputTask = document.getElementById("input-task")
-inputTask.addEventListener('keydown', (event) => { addTaskByEnter(event.key, "input-task", "list-tasks", Date.now()) })
+const inputTask = document.getElementById("input-task");
+inputTask.addEventListener('keydown', (event) => { addTaskByEnter(event.key, "input-task", "list-tasks") });
