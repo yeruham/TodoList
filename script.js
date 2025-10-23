f9afd5a27c387ae2644ed344ce46b23272ef4adb
YeruhamMendelson <y0583207549@gmail.com>
2025-10-23 14:30:49 +0300

    func to localStorage - i added them to all buttons

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function createDeleteButton(){
     newDeleteElement.setAttribute("type", "submit");
     newDeleteElement.className = "delete-task";
     newDeleteElement.innerText = "delete";
-    newDeleteElement.addEventListener('click', (e) => {  e.target.parentElement.remove(); })
+    newDeleteElement.addEventListener('click', (e) => { deleteTask(e.target.parentElement) })
     return newDeleteElement;
 }
 
@@ -41,7 +41,7 @@ function createEditButton(){
 
 
 function createTaskElement(text){
-    const taskElement = createTextElement(text);
+    const textElement = createTextElement(text);
     const editElement = createEditButton();
     const deleteElement = createDeleteButton();
     const doneElement = createDoneButton();
@@ -49,7 +49,7 @@ function createTaskElement(text){
     const fullTaskElement = document.createElement("li");
     fullTaskElement.className = "task";
     
-    fullTaskElement.appendChild(taskElement);
+    fullTaskElement.appendChild(textElement);
     fullTaskElement.appendChild(editElement);
     fullTaskElement.appendChild(deleteElement);
     fullTaskElement.appendChild(doneElement);
@@ -75,6 +75,8 @@ function addNewTask(inputId, listTasksId){
     const listTasks = document.getElementById(listTasksId);
     const newTaskElement = createTaskElement(newTask);
     listTasks.appendChild(newTaskElement);
+
+    newTaskElement.id = addValueToLocalStorage(listTasksId, newTask)
 }
 
 
@@ -99,6 +101,11 @@ function editTaskByKeys(e){
         if (e.key == "Enter"){
             currentTextElement.innerText = editButton.value;
             editButton.replaceWith(currentTaskElement);
+            
+            const localStorageKey = currentTaskElement.parentElement.id;
+            const taskId = currentTaskElement.id;
+            editValueInLocalStorage(localStorageKey, taskId, editButton.value);
+
         }else if (e.key == "Escape"){
             editButton.replaceWith(currentTaskElement);
         }else{
@@ -109,8 +116,24 @@ function editTaskByKeys(e){
 
 
 function moveDoneTask(taskElement){
+    let localStorageKey = taskElement.parentElement.id
+    console.log(localStorageKey)
+    deleteValueFromLocalStorage(localStorageKey, taskElement.id)
+
     const doneTasks = document.getElementById("done-tasks");
     doneTasks.appendChild(taskElement);
+
+    localStorageKey = doneTasks.id;
+    const textTask = taskElement.getElementsByClassName("text-task")[0].innerText;
+    taskElement.id = addValueToLocalStorage(localStorageKey, textTask);
+}
+
+
+function deleteTask(taskElement){
+    const taskId = taskElement.id
+    const localStorageKey = taskElement.parentElement.id
+    taskElement.remove();
+    deleteValueFromLocalStorage(localStorageKey, taskId);
 }
 
 
@@ -119,6 +142,62 @@ function deleteTasks(listTasksId){
     doneTasks.innerHTML = "";
 }
 
+
+function addValueToLocalStorage(key, value){
+    let listOfValues = localStorage.getItem(key);
+    const valueId = Date.now()
+    const localStorageValue = {"task": value, "id": valueId};
+    if (listOfValues == null){
+        newList = [];
+        newList.push(localStorageValue);
+        newList = JSON.stringify(newList);
+        localStorage.setItem(key, newList);
+    }
+    else{
+        listOfValues = JSON.parse(listOfValues);
+        listOfValues.push(localStorageValue);
+        listOfValues = JSON.stringify(listOfValues);
+        localStorage.setItem(key, listOfValues);
+    }
+    return valueId
+}
+
+
+function deleteValueFromLocalStorage(key, id){
+    let listOfValues = localStorage.getItem(key);
+    if (listOfValues == null){
+        return;
+    }
+    listOfValues = JSON.parse(listOfValues);
+    for (let i = 0; i < listOfValues.length; i++){
+        if (listOfValues[i].id == id){
+            const deletedValue = listOfValues.splice(i, 1);
+        }
+    }
+    listOfValues = JSON.stringify(listOfValues);
+    localStorage.setItem(key, listOfValues);
+}
+
+
+function editValueInLocalStorage(key, id, newValue){
+    let listOfValues = localStorage.getItem(key);
+    if (listOfValues == null){
+        return;
+    }
+    listOfValues = JSON.parse(listOfValues);
+    for (let i = 0; i < listOfValues.length; i++){
+        console.log(listOfValues[i])
+        if (listOfValues[i].id == id){
+            listOfValues[i].task = newValue;
+        }
+    }
+    listOfValues = JSON.stringify(listOfValues);
+    localStorage.setItem(key, listOfValues);
+}
+
+
+const tasksKey = "tasks"
+const doneTasksKey = "done-tasks"
 
 
 const saveNewTask = document.getElementById("add-button");
@@ -130,8 +209,8 @@ inputTask.addEventListener('keydown', (event) => { addTaskByEnter(event.key, "in
 
 
 const deleteAll = document.getElementById("delete-all-tasks");
-deleteAll.addEventListener('click', () => { deleteTasks("todo-tasks"); deleteTasks("done-tasks"); });
+deleteAll.addEventListener('click', () => { deleteTasks("todo-tasks"); deleteTasks("done-tasks"); localStorage.clear(); });
 
 
 const deleteDoneTasks = document.getElementById("delete-done-tasks");
-deleteDoneTasks.addEventListener('click', () => { deleteTasks("done-tasks"); });
+deleteDoneTasks.addEventListener('click', () => { deleteTasks("done-tasks"); localStorage.removeItem(doneTasksKey); });
