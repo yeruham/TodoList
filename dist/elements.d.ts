e5dbff6cd48086c73ff34f7953ece2c63b76a2c5
yeruham <y0583207549@gmail.com>
2025-10-28 17:20:46 +0200

    Delete dist directory

--- dist/elements.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function createTextElement(text: string): HTMLSpanElement;
-export declare function createDeleteButton(): HTMLButtonElement;
-export declare function createDoneButton(done?: boolean): HTMLButtonElement;
-export declare function createEditButton(): HTMLButtonElement;
-export declare function createTaskFrame(isDone?: boolean): HTMLLIElement;
-export declare function createTaskElement(text: string, done?: boolean): HTMLLIElement;
-//# sourceMappingURL=elements.d.ts.map
--- dist/taskLocalStorage.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function addValue(isDone: boolean, value: string): string;
-export declare function editValue(id: string, newValue: string): void;
-export declare function deleteTasks(done: boolean): void;
-export declare function tasksFromLocalStorage(listTasksId: string, listDoneTasksId: string): void;
-export declare function updateOrderTasks(done: boolean, tasks: HTMLCollection): void;
-//# sourceMappingURL=taskLocalStorage.d.ts.map
