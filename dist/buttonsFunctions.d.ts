7c0d08fac58bdfcef554df7cd541db308bdd62dc
YeruhamMendelson <y0583207549@gmail.com>
2025-10-28 17:16:44 +0200

    i converted everything to ts

---
+++ dist/buttonsFunctions.d.ts
@@ -0,0 +1,7 @@
+export declare function editTaskByKeys(taskElement: HTMLElement): void;
+export declare function moveDoneTask(taskElement: HTMLElement): void;
+export declare function deleteTask(taskElement: HTMLElement): void;
+export declare function displayEditButton(taskElement: HTMLElement): void;
+export declare function nonDisplayEditButton(taskElement: HTMLElement): void;
+export declare function styleOfTaskElement(taskElement: HTMLElement, normal?: boolean): void;
+//# sourceMappingURL=buttonsFunctions.d.ts.map
---
+++ dist/elements.d.ts
@@ -0,0 +1,7 @@
+export declare function createTextElement(text: string): HTMLSpanElement;
+export declare function createDeleteButton(): HTMLButtonElement;
+export declare function createDoneButton(done?: boolean): HTMLButtonElement;
+export declare function createEditButton(): HTMLButtonElement;
+export declare function createTaskFrame(isDone?: boolean): HTMLLIElement;
+export declare function createTaskElement(text: string, done?: boolean): HTMLLIElement;
+//# sourceMappingURL=elements.d.ts.map
